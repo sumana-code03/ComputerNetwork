71af06a0bbfab56e47a63c02bd701b1a32038059
sumana-code03 <sumanamondal3434@gmail.com>
2026-07-17 23:12:05 +0530

    UDP calculator client

---
+++ User Datagram Protocol/Calculator/client.c
@@ -0,0 +1,39 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define PORT 25050
+char buff[128];
+int main(){
+    int sockfd, r;
+    struct sockaddr_in serv_addr;
+    socklen_t len = sizeof(serv_addr);
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    inet_aton("127.0.0.1", &serv_addr.sin_addr);
+    printf("\nUDP SIMPLE CALCULATOR CLIENT\n");
+    while(1){
+        printf("\nEnter expression (e.g., 5 + 3) or exit: ");
+        fgets(buff, sizeof(buff), stdin);
+        sendto(sockfd, buff, strlen(buff), 0,
+               (struct sockaddr*)&serv_addr, len);
+        if(strncmp(buff, "exit", 4) == 0){
+            printf("Client exiting.\n");
+            break;
+        }
+        r = recvfrom(sockfd, buff, sizeof(buff), 0,(struct sockaddr*)&serv_addr, &len);
+        buff[r] = '\0';
+
+        if(strncmp(buff, "SERVER_EXIT", 11) == 0){
+            printf("Server requested shutdown. Exiting...\n");
+            break;
+        }
+        printf("RESULT: %s\n", buff);
+    }
+    close(sockfd);
+    return 0;
+}
